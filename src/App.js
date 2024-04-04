6f10f3199fc5b7eccdfe6c14669f50740345c9ff
Akalynsia <arminasmirage@gmail.com>
2024-04-04 12:48:06 +0300

    interview

--- src/App.js
+++ src/App.js
@@ -8,36 +8,41 @@ const WordByWord = () => {
   // KODUNUZ BURAYA GELECEK
   const [text, setText] = useState("");
   const [words, setWords] = useState([]);
+  const [currentWordIndex, setCurrentWordIndex] = useState(0);
 
   useEffect(() => {
     setWords(text.split(" "));
+    setCurrentWordIndex(0);
   }, [text]);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (words.length > 0) {
-        console.log(words[0]);
-        setWords((prevWords) => prevWords.slice(1));
-      }
-    }, 500);
-
-    return () => clearInterval(interval);
-  }, [words]);
+    if (currentWordIndex < words.length) {
+      console.log(words[currentWordIndex]);
+    }
+  }, [currentWordIndex, words]);
 
   const handleInputChange = (event) => {
     setText(event.target.value);
-    setWords([]);
   };
 
+  const handleNextWordClick = () => {
+    setCurrentWordIndex((prevIndex) => prevIndex + 1);
+  };
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex flex-col justify-center items-center h-screen">
       <input
         type="text"
         value={text}
         onChange={handleInputChange}
-        className="border border-gray-300 rounded px-4 py-2 focus:outline-none focus:border-blue-500"
+        className="border border-gray-300 rounded px-4 py-2 mb-4 focus:outline-none focus:border-blue-500"
         placeholder="Type your text here..."
       />
+      <button
+        onClick={handleNextWordClick}
+        className="bg-blue-500 text-white px-4 py-2 rounded focus:outline-none"
+      >
+        Next
+      </button>
     </div>
   );
 };
